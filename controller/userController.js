60688e1c64469dbbea1d2b257d3ebd40275b26f7
soniagkalliath <soniageorge111@gmail.com>
2025-12-08 12:56:07 +0530

    ADD: login & jsonwebtoken

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const users = require('../models/userModel')
+//jsonwebtoken
+const jwt = require('jsonwebtoken')
 
 //register api request
 exports.registerController = async (req,res)=>{
@@ -23,5 +25,28 @@ exports.registerController = async (req,res)=>{
     }
 }
 //login api
+exports.loginController = async (req,res)=>{
+    console.log("Inside loginController");
+    const {email,password} = req.body
+    console.log(email,password);
+    try{
+        //check mail in model
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            if(password == existingUser.password){
+                //generate token
+                const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)
+                res.status(200).json({user:existingUser,token})
+            }else{
+                res.status(401).json("Incorrect Email / Password")
+            }
+        }else{
+            res.status(404).json("Account Doesnot Exists!!!")
+        }
+    }catch(error){
+        console.log(error);        
+        res.status(500).json(error)
+    }
+}
 //usereditprofile
 //admineditprofile
--- routes/routing.js
+++ routes/routing.js
@@ -8,6 +8,7 @@ const router = new express.Router()
 //define path for  client api request
 //register
 router.post('/register',userController.registerController)
-
+//login
+router.post('/login',userController.loginController)
 
 module.exports = router
